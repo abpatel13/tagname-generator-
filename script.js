b99c4b8e0631d2231d95131b32e5755fbfe9d4e3
abpatel13 <151817708+abpatel13@users.noreply.github.com>
2024-07-27 20:24:44 +0530

    first comit

---
+++ script.js
@@ -0,0 +1,12 @@
+const input = document.querySelector('.input')
+const container  = document.querySelector('.container')
+const array = []
+
+
+
+function generateTagname(){
+    const name = input.value
+
+
+
+}
